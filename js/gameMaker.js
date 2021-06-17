eda6abdfa363f90cfd1a1b3ffff3ada8df176aef
Minhyuk Oh <maothesolution@gmail.com>
2021-06-18 03:29:22 +0900

    a nav bar is made. buttons are made. PLAY&PAUSE button is made

--- js/gameMaker.js
+++ js/gameMaker.js
@@ -1,1 +1,8 @@
-console.log("hi")
+function play(){
+    var temp = document.getElementById("playBtn").textContent;
+    if(temp === "PLAY"){
+        document.getElementById("playBtn").innerText="PAUSE";
+    } else {
+        document.getElementById("playBtn").innerText="PLAY";
+    }
+}
