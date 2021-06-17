1d3db2c3e97b69a10ae46b337cd1423ae60130f1
Minhyuk Oh <maothesolution@gmail.com>
2021-06-18 08:18:47 +0900

    1. adjust the size of the cards in css 2. limit the the number of the cards
    different levels will be provided to choose a different number of cards

--- js/gameMaker.js
+++ js/gameMaker.js
@@ -1,3 +1,6 @@
+/**
+ * PLAY and PAUSE buttons can be switched by clicking it
+ */
 function play(){
     var temp = document.getElementById("playBtn").textContent;
     if(temp === "PLAY"){
@@ -5,4 +8,28 @@ function play(){
     } else {
         document.getElementById("playBtn").innerText="PLAY";
     }
+    makeTable();
 }
+
+/**
+ * make a card table, 8 rows * 13 columns, with the back of cards 
+ */
+function makeTable(){
+    var cardTable = "";
+    cardTable += "<table class='table'>";
+    for(var row=0; row<5; row++){
+        cardTable += "<tr>";
+        var symbol = "";
+        for(var col=0; col<10; col++){
+            cardTable += "<td>" + "<img src='./img/back.png'>" + "</td>"
+        }
+        cardTable += "</tr>"
+    }
+    cardTable += "</table>";
+    document.getElementById("cardTable").innerHTML = cardTable;
+}
+
+function pickRandomCard(){
+    
+}
+
